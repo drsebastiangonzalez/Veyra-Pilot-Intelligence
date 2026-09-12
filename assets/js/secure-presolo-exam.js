90c499e6c1ce713612142329f0cc16fc0a3cd14f
drsebastiangonzalez <dr.sebastian.gonzalez.chaves@gmail.com>
2026-09-11 19:29:35 -0500

    Bump Veyra operation loader version

--- assets/js/secure-presolo-exam.js
+++ assets/js/secure-presolo-exam.js
@@ -78,7 +78,7 @@
 
   if(!document.querySelector('script[data-veyra-operation]')){
     const showcase=document.createElement('script');
-    showcase.src='assets/js/veyra-en-operacion.js?v=1';
+    showcase.src='assets/js/veyra-en-operacion.js?v=2';
     showcase.defer=true;
     showcase.dataset.veyraOperation='true';
     document.head.appendChild(showcase);
